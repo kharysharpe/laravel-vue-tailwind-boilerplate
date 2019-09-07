76f76c106784cd7a8547fe2b322869a2138f7337
Khary Sharpe <kharysharpe@gmail.com>
2019-09-07 15:39:54 -0500

    Switched to mix for versioning

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,6 @@
 <body>
     <div id="app"></div>
     <!-- Inject Vue -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
